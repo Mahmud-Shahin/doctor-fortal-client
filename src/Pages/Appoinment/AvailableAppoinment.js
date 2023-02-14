50701145d709d7ceeed0b033289b6533f6214d9e
Mahmudshahin <Shahinmahmud184@gmail.com>
2023-02-14 09:12:28 +0600

    data update

--- src/Pages/Appoinment/AvailableAppoinment.js
+++ src/Pages/Appoinment/AvailableAppoinment.js
@@ -1,12 +1,24 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Booking from "./Booking";
 
 const AvailableAppoinment = ({ date }) => {
+  const [bookings, setBookings] = useState([]);
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setBookings(data));
+  }, []);
   return (
     <div>
       <h3 className="text-xl text-secondary text-center">
         Available Appoinment on {format(date, "PP")}
       </h3>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10">
+        {bookings.map((booking) => (
+          <Booking key={booking.id} booking={booking}></Booking>
+        ))}
+      </div>
     </div>
   );
 };
