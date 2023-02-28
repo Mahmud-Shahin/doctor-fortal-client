c59c1fb38910033ed61aaabe12cc2dc8bedac7bc
Mahmudshahin <Shahinmahmud184@gmail.com>
2023-02-28 09:17:16 +0600

    update data

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const Login = () => {
+  const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+
+  if (user) {
+    console.log(user);
+  }
   return (
-    <div>
-      <h1>please login here</h1>
+    <div className="flex justify-center items-center h-screen">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center font-bold text-2xl">Login</h2>
+          <div className="divider">OR</div>
+          <button
+            onClick={() => signInWithGoogle()}
+            className="btn btn-outline"
+          >
+            Continue with google
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
