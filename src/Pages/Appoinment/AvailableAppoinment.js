b0d0b79d600f82658bcbd64bd38b48968404426d
Mahmudshahin <Shahinmahmud184@gmail.com>
2023-04-16 23:58:33 +0600

    update data

--- src/Pages/Appoinment/AvailableAppoinment.js
+++ src/Pages/Appoinment/AvailableAppoinment.js
@@ -2,23 +2,39 @@ import { format } from "date-fns";
 import React, { useState, useEffect } from "react";
 import Booking from "./Booking";
 import BookingModal from "./BookingModal";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
 
 const AvailableAppoinment = ({ date }) => {
-  const [bookings, setBookings] = useState([]);
+  // const [bookings, setBookings] = useState([]);
   const [treatment, setTreatment] = useState(null);
   const formattedDate = format(date, "pp");
-  useEffect(() => {
-    fetch(`http://localhost:5000/available?date=${formattedDate}`)
-      .then((res) => res.json())
-      .then((data) => setBookings(data));
-  }, [formattedDate]);
+  const {
+    data: bookings,
+    isLoading,
+    refetch,
+  } = useQuery(["available", formattedDate], () =>
+    fetch(`http://localhost:5000/available?date=${formattedDate}`).then((res) =>
+      res.json()
+    )
+  );
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
+  // useEffect(() => {
+  //   fetch(`http://localhost:5000/available?date=${formattedDate}`)
+  //     .then((res) => res.json())
+  //     .then((data) => setBookings(data));
+  // }, [formattedDate]);
+
   return (
     <div>
       <h3 className="text-xl text-secondary text-center">
         Available Appoinment on {format(date, "PP")}
       </h3>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10">
-        {bookings.map((booking) => (
+        {bookings?.map((booking) => (
           <Booking
             key={booking._id}
             booking={booking}
@@ -31,6 +47,7 @@ const AvailableAppoinment = ({ date }) => {
           date={date}
           treatment={treatment}
           setTreatment={setTreatment}
+          refetch={refetch}
         ></BookingModal>
       )}
     </div>
