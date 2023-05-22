7206e99951ac9887529f71503aee6baf0a6fde65
Mahmudshahin <Shahinmahmud184@gmail.com>
2023-05-22 22:56:10 +0600

    update data

--- src/Pages/Dashboard/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor.js
@@ -1,14 +1,46 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useQuery } from "react-query";
+import Loading from "./../Shared/Loading";
 const AddDoctor = () => {
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
+
+  const { data: services, isLoading } = useQuery("services", () =>
+    fetch("http://localhost:5000/service").then((res) => res.json())
+  );
+
+  const imageStoragekey = "935664776886cd731b55e1fadc37410d";
+
   const onSubmit = async (data) => {
-    console.log("data", data);
+    const image = data.image[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    const url = `https://api.imgbb.com/1/upload?key=${imageStoragekey}`;
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.success) {
+          const img = result.data.url;
+          const Doctor = {
+            name: data.name,
+            email: data.email,
+            speciality: data.speciality,
+            img: img,
+          };
+        }
+      });
   };
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
       <h2 className="text-2xl"> Add a Doctor </h2>
@@ -74,26 +106,37 @@ const AddDoctor = () => {
           <label className="label">
             <span className="label-text">specialization</span>
           </label>
+          <select
+            {...register("specialization")}
+            className="select input-bordered w-full max-w-xs"
+          >
+            {services.map((service) => (
+              <option key={service._Id} value={service.name}>
+                {service.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Image</span>
+          </label>
           <input
-            type="text"
-            placeholder="specialization"
+            type="file"
+            placeholder="you photo"
             className="input input-bordered w-full max-w-xs"
-            {...register("specialization", {
+            {...register("image", {
               required: {
                 value: true,
-                message: " specialization is required",
+                message: " image  is required",
               },
             })}
           />
           <label className="label">
-            {errors.password?.type === "required" && (
+            {errors.name?.type === "required" && (
               <span className="label-text-alt text-red-500">
-                {errors.password.message}
-              </span>
-            )}
-            {errors.password?.type === "minLength" && (
-              <span className="label-text-alt text-red-500">
-                {errors.password.message}
+                {errors.name.message}
               </span>
             )}
           </label>
